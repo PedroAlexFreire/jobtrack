a460bdf5ad1959a1a57d9aad16758241661bed06
Pedro Freire <pedroalexfreire@gmail.com>
2026-08-06 20:10:51 +0100

    Add endpoint to find application by id

--- src/main/java/com/jobtrack/jobtrack/controller/JobApplicationController.java
+++ src/main/java/com/jobtrack/jobtrack/controller/JobApplicationController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,5 +33,16 @@ public class JobApplicationController {
     public JobApplication addApplication(@RequestBody JobApplication application) {
         this.applications.add(application);
         return application;
+    }
+    @GetMapping("/api/applications/{id}")
+    public ResponseEntity<JobApplication> getApplicationById(@PathVariable Long id) {
+
+        for (JobApplication application : this.applications) {
+            if (application.getId().equals(id)) {
+                return ResponseEntity.ok(application);
+            }
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
